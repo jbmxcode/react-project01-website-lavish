2c06c768fcd1df060618347cb89cba834e3f7460
jbmxworld@gmail.com <jbmxworld@gmail.com>
2020-10-03 06:12:59 -0500

    Updates

---
+++ precache-manifest.6cb84cfd42c2f32231d3b563a2185187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3895b957771ba529dfd29e1a22ea5724",
+    "url": "/react-project01-website-lavish/index.html"
+  },
+  {
+    "revision": "2c5763efa21b1e329b60",
+    "url": "/react-project01-website-lavish/static/css/main.0779d1c8.chunk.css"
+  },
+  {
+    "revision": "95d5c5d68ee2852ff2b9",
+    "url": "/react-project01-website-lavish/static/js/2.f2c98645.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-project01-website-lavish/static/js/2.f2c98645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5763efa21b1e329b60",
+    "url": "/react-project01-website-lavish/static/js/main.8127a3dd.chunk.js"
+  },
+  {
+    "revision": "0eab9acc11fefdf28a60",
+    "url": "/react-project01-website-lavish/static/js/runtime-main.6e9859a4.js"
+  }
+]);
